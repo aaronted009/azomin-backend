b25699ad13aed4e015071458e7a126d3f6d59149
Aaron <aaronpromail@gmail.com>
2024-10-15 15:23:16 +0100

    update database creation with SQLModel and Session creation as well

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlmodel import create_engine, Session, SQLModel
+from fastapi import Depends
+from typing import Annotated
 from dotenv import load_dotenv
 import os
 
@@ -13,9 +13,20 @@ database_name = os.environ.get("DATABASE_NAME")
 database_host = os.environ.get("DATABASE_HOST")
 database_port = os.environ.get("DATABASE_PORT")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{database_host}:{database_port}/{database_name}"
+SQLALCHEMY_DATABASE_URL = (
+    f"postgresql://{user}:{password}@{database_host}:{database_port}/{database_name}"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
