be3424cb86121217bbe2ae3ac7b7437658d5506d
Aaron <aaronpromail@gmail.com>
2024-11-22 00:05:14 +0100

    make models accessible to other modules

---
+++ app/models/__init__.py
@@ -0,0 +1,12 @@
+from .classroom import ClassRoom
+from .course import Course
+from .exam_result import ExamResult
+from .exam import Exam
+from .fee import Fee
+from .person import Person
+from .student_tutor import StudentTutor
+from .student import Student
+from .teacher import Teacher
+
+
+__all__ = [ClassRoom, Course, ExamResult, Exam, Fee, Person, StudentTutor, Student, Teacher]
