9234a35980025530d8838563ebe43a2f2862cbc8
Aaron <aaronpromail@gmail.com>
2024-11-21 21:19:48 +0100

    update Exam model with exam_results relationship

--- app/models/exam.py
+++ app/models/exam.py
@@ -1,5 +1,7 @@
 from datetime import date
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+from .exam_result import ExamResult
+from typing import Optional
 
 
 class Exam(SQLModel, table=True):
@@ -9,3 +11,4 @@ class Exam(SQLModel, table=True):
     trimester: int = Field()
     classroom_id: int = Field(foreign_key="classroom.id")
     course_id: int = Field(foreign_key="course.id")
+    exam_results: Optional[ExamResult] = Relationship(back_populates="exam")
