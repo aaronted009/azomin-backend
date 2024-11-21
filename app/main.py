2343934cdf4f28f765d82f7d3ecf4a1792dbe5c5
Aaron <aaronpromail@gmail.com>
2024-11-22 00:01:38 +0100

    add API endpoints (wip)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from .models import Course, Teacher
+from fastapi import APIRouter, FastAPI
+from fastapi import HTTPException
 from .database import create_db_and_tables
 
 app = FastAPI()
@@ -12,3 +14,37 @@ def on_startup():
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+router = APIRouter()
+
+
+@router.get("/courses/{course_id}", response_model=Course)
+async def read_course(course_id: int):
+    course = await Course.get(course_id)
+    if course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return course
+
+
+@router.post("/courses/", response_model=Course)
+async def create_course(course: Course):
+    await course.save()
+    return course
+
+
+@router.get("/teachers/{teacher_id}", response_model=Teacher)
+async def read_teacher(teacher_id: int):
+    teacher = await Teacher.get(teacher_id)
+    if teacher is None:
+        raise HTTPException(status_code=404, detail="Teacher not found")
+    return teacher
+
+
+@router.post("/teachers/", response_model=Teacher)
+async def create_teacher(teacher: Teacher):
+    await teacher.save()
+    return teacher
+
+
+app.include_router(router)
