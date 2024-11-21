defe2b95a29122f18add1c462e149957127b365e
Aaron <aaronpromail@gmail.com>
2024-11-21 21:27:35 +0100

    update StudentTutor.student relationship by updating back_populates attribute value (student to student_tutors)

--- app/models/student_tutor.py
+++ app/models/student_tutor.py
@@ -5,5 +5,5 @@ from sqlmodel import Field, Relationship
 class StudentTutor(Person, table=True):
     id: int = Field(primary_key=True)
     student_id: int = Field(foreign_key="student.id")
-    student: Student = Relationship(back_populates="student")
+    student: Student = Relationship(back_populates="student_tutors")
     
